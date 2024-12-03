9d23b60a19d5eb938919a94f8b851c1be61987e5
RLE1058 <rgurram@hbs.edu>
2024-12-03 15:37:05 +0530

    script updated

--- products/ec2-secure-windows/provision-windows.ps1
+++ products/ec2-secure-windows/provision-windows.ps1
@@ -1,161 +1,92 @@
+  
 # Redirect stdout and stderr to a log file and the console
-$scriptPath = "C:\Users\Administrator\log\provision-windows.log"
-Start-Transcript -Path $scriptPath -Append  
+# $scriptPath = "C:\Users\Administrator\log\provision-windows.log"
+# Start-Transcript -Path $scriptPath -Append  
 
-# Add mount script and set-user-token to startup folder
-Write-Host "Moving mount-s3.bat and set-user-token.bat to startup folder"
-if (Test-Path -Path "C:\Program Files\ResearchGateway\mount_s3.bat") {
-	Copy-Item -Path "C:\Program Files\ResearchGateway\default.perm" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\"
- -Force
-} elseif (Test-Path -Path "C:\Users\Administrator\mount_s3.bat") {
-	// This is only to support older pipelines which expect this location of the file
-	Copy-Item -Path "C:\Users\Administrator\mount_s3.bat" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\"
- -Force
-}else {
-	Write-Host "Error: Could not find mount_s3.bat. Study mounting might not work correctly."		
-}
-if (Test-Path -Path "C:\Program Files\ResearchGateway\set_user_token.bat") {
-	Copy-Item -Path "C:\Program Files\ResearchGateway\set_user_token.bat" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\"
- -Force
-} elseif (Test-Path -Path "C:\Users\Administrator\set_user_token.bat") {
-	// This is only to support older pipelines which expect this location of the file
-	Copy-Item -Path "C:\Users\Administrator\set_user_token.bat" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\"
- -Force
-}else {
-	Write-Host "Error: Could not find set_user_token.bat. Launch URL might not work correctly."		
-}
-
-
-
-# Set Administrator password
-net user Administrator Admin@123
-
-# Set environment variable
+# Set env variable
 setx PARAMNAMEPREFIX /RL/RG/secure-desktop/auth-token/
-
-# Download and install Microsoft Visual C++ Redistributable for Visual Studio 2017
-Write-Host "Downloading Microsoft Visual C++ Redistributable for Visual Studio 2017..."
-$downloadsuccesful = $false
-Invoke-WebRequest -Uri "https://aka.ms/vs/15/release/vc_redist.x64.exe" -OutFile "$env:TEMP\vc_redist.x64.exe"
-$downloadsuccesful = Test-Path -Path "$env:TEMP\vc_redist.x64.exe"
-
-if ($downloadsuccesful) {
-	Write-Host "Installing Microsoft Visual C++ Redistributable for Visual Studio 2017"
-	Start-Process -FilePath "$env:TEMP\vc_redist.x64.exe" -ArgumentList "/install /quiet /norestart" -Wait
-	Remove-Item -Path "$env:TEMP\vc_redist.x64.exe" -Force
-} else {
-	Write-Host "Failed to download Microsoft Visual C++ Redistributable for Visual Studio 2017"
-	Exit 1
-}
-
+ 
 # AWS CLI Installer URL
 $awsCliUrl = "https://awscli.amazonaws.com/AWSCLIV2.msi"
 
+echo "downloading AWS-CLI..." 
 # Download AWS CLI MSI
-Write-Host "Downloading AWS-CLI...$awsCliUrl"
-$downloadsuccesful = $false
 Invoke-WebRequest -Uri $awsCliUrl -OutFile "$env:TEMP\AWSCLIV2.msi"
-$downloadsuccesful = Test-Path -Path "$env:TEMP\AWSCLIV2.msi"
+
+# Download and install Microsoft Visual C++ Redistributable for Visual Studio 2017
+echo "downloading Microsoft Visual C++ Redistributable for Visual Studio 2017..." 
+Invoke-WebRequest -Uri "https://aka.ms/vs/15/release/vc_redist.x64.exe" -OutFile "$env:TEMP\vc_redist.x64.exe"
+Start-Process -FilePath "$env:TEMP\vc_redist.x64.exe" -ArgumentList "/install /quiet /norestart" -Wait
 
 # Install AWS CLI silently
-if ($downloadsuccesful) {
-	Write-Host "Installing AWS-CLI" 
-	Start-Process -FilePath msiexec.exe -ArgumentList "/i `"$env:TEMP\AWSCLIV2.msi`" /quiet /qn /norestart" -Wait	
-	Remove-Item -Path "$env:TEMP\AWSCLIV2.msi" -Force
-} else {
-	Write-Host "AWS CLI download failed"
-	Exit 1
-}
+echo "Installing AWS-CLI" 
+Start-Process -FilePath msiexec.exe -ArgumentList "/i `"$env:TEMP\AWSCLIV2.msi`" /quiet /qn /norestart" -Wait
 
-# Download Nice-Dcv Server
-Write-Host "Downloading Nice-Dcv"
-$downloadsuccesful = $false
+# Remove temporary files
+echo "Removing AWSCLIV2.msi, vc_redist.x64.exe" 
+Remove-Item "$env:TEMP\AWSCLIV2.msi" -Force
+Remove-Item "$env:TEMP\vc_redist.x64.exe" -Force
+
+# Download nice-dcv-server
+echo "Downloading Nice-Dcv" 
 $niceDcvServerURL = "https://d1uj6qtbmh3dt5.cloudfront.net/2023.1/Servers/nice-dcv-server-x64-Release-2023.1-16388.msi"
-Invoke-WebRequest -Uri $niceDcvServerURL -OutFile "$env:TEMP\nice-dcv-server-x64-Release-2023.1-16388.msi"
-$downloadsuccesful = Test-Path -Path "$env:TEMP\nice-dcv-server-x64-Release-2023.1-16388.msi"
-$restartdcv = $false
-if (!$downloadsuccesful){
-	Write-Host "Failed to download NICE DCV Server"
-	Exit 1
-}	
-# Install Nice-Dcv Server
-Write-Host "Installing Nice-Dcv Server"
-Start-Process msiexec.exe -ArgumentList "/i `"$env:TEMP\nice-dcv-server-x64-Release-2023.1-16388.msi`" ADDLOCAL=ALL /quiet /norestart /l*v 	dcv_install_msi.log" -Wait
-Remove-Item -Path "$env:TEMP\nice-dcv-server-x64-Release-2023.1-16388.msi" -Force
-Write-Host "Moving default.perm to location C:\Program Files\NICE\DCV\Server\conf\default.perm"
-if (Test-Path -Path "C:\Program Files\ResearchGateway\default.perm") {
-	Copy-Item -Path "C:\Program Files\ResearchGateway\default.perm" -Destination "C:\Program Files\NICE\DCV\Server\conf\default.perm" -Force
-	$restartdcv = $true
-} elseif (Test-Path -Path "C:\Users\Administrator\default.perm") {
-	// This is only to support older pipelines which expect this location of the file
-	Copy-Item -Path "C:\Users\Administrator\default.perm" -Destination "C:\Program Files\NICE\DCV\Server\conf\default.perm" -Force
-	$restartdcv = $true
-}else {
-	Write-Host "Error: Could not find default.perm. NICE DCV might not be configured correctly."		
-}
+Invoke-WebRequest -Uri $niceDcvServerURL -OutFile "C:\Users\Administrator\nice-dcv-server-x64-Release-2023.1-16388.msi"
 
-if ($restartdcv) {
-	Write-Host "Restarting DCV server"
-	Restart-Service "dcvserver"
-	$svcstatus= ((Get-Service -Name 'dcvserver').Status )
-	Write-Host "DCV service status:  $svcstatus"
-}
-# Download and install rclone
-Write-Host "Downloading rclone..."
-mkdir C:\rclone
+# install nice-dcv-server
+echo "Installing nice-dcv-server"
+Start-process msiexec.exe -wait -ArgumentList '/i nice-dcv-server-x64-Release-2023.1-16388.msi ADDLOCAL=ALL /quiet /norestart /l*v dcv_install_msi.log'
+
+# Install Rstudio
+echo "Downloading Rstudio..."
+$rstudioURL = "https://cran.rstudio.com/bin/windows/base/R-4.4.1-win.exe"
+Invoke-WebRequest -Uri $rstudioURL -OutFile "C:\Users\Administrator\R-4.4.1-win.exe"
+echo "Installing Rstudio..."
+.\R-4.4.1-win.exe /VERYSILENT /NORESTART
+
+# Install rclone
+echo "Downloading rclone..."
+mkdir c:\rclone
 $rcloneURL = "https://downloads.rclone.org/v1.65.2/rclone-v1.65.2-windows-amd64.zip"
-$downloadsuccesful = $false
-Invoke-WebRequest -Uri $rcloneURL -OutFile "$env:TEMP\rclone-v1.65.2-windows-amd64.zip"
-$downloadsuccesful = Test-Path -Path "$env:TEMP\rclone-v1.65.2-windows-amd64.zip"
-if (!$downloadsuccesful) {
-	Write-Host "Failed to download rclone. Mounting studies may not work."
-	Exit 1
-}
-Write-Host "Installing rclone..."
-Expand-Archive -Path "$env:TEMP\rclone-v1.65.2-windows-amd64.zip" -DestinationPath "C:\rclone\"
-Remove-Item -Path "$env:TEMP\rclone-v1.65.2-windows-amd64.zip" -Force
-# Download and install RStudio
-Write-Host "Downloading RStudio..."
-$RVersion = "R-4.4.1-win.exe"
-$rstudioURL = "https://cran.rstudio.com/bin/windows/base/$RVersion"
-$downloadsuccesful = $false
-Invoke-WebRequest -Uri $rstudioURL -OutFile "$env:TEMP\$RVersion"
-$downloadsuccesful = Test-Path -Path "$env:TEMP\$RVersion"
+Invoke-WebRequest -Uri $rcloneURL -OutFile "C:\Users\Administrator\rclone-v1.65.2-windows-amd64.zip"
 
-if ($downloadsuccesful) {
-	Write-Host "Installing RStudio..."
-	Start-Process -FilePath "$env:TEMP\$RVersion" -ArgumentList "/VERYSILENT /NORESTART" -Wait
-	Remove-Item -Path "$env:TEMP\$RVersion" -Force
-} else {
-	Write-Host "Failed to download RStudio"
-}
+echo "Installing Rclone"
+Expand-Archive -path 'C:\Users\Administrator\rclone-v1.65.2-windows-amd64.zip' -destinationpath '.\'
+cp C:\Users\Administrator\rclone-v1.65.2-windows-amd64\*.* C:\rclone\
 
-# Install Chocolatey and winfsp
-Write-Host "Installing Chocolatey..."
-Set-ExecutionPolicy Bypass -Scope Process -Force
-iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1"))
+echo "Installing chocollaty and Google-Chrome..."
+Set-ExecutionPolicy Bypass -Scope Process -Force; `iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install winfsp -y
+choco install googlechrome -y
 
 # Install cx_Freeze
-Write-Host "Installing cx_Freeze..."
-cmd /c start powershell -ExecutionPolicy RemoteSigned {
-    pip install cx_Freeze
+echo "Instaling cx_Freeze"
+cmd /c start powershell -executionpolicy remotesigned {
+pip install cx_Freeze
 }
 
-# Install Node.js
-Write-Host "Downloading Node.js..."
+#Compile custom auth svc
+echo "compiling custom auth service"
+C:\Users\Administrator\win-nice-dcv-auth-svc\install_custom_auth.bat
+
+
+#install node.js
+echo "Installing Node"
 $nodeURL = "https://nodejs.org/dist/v18.20.2/node-v18.20.2-x64.msi"
-$downloadsuccesful = $false
-Invoke-WebRequest -Uri $nodeURL -OutFile "$env:TEMP\node-v18.20.2-x64.msi"
-$downloadsuccesful = Test-Path -Path "$env:TEMP\node-v18.20.2-x64.msi"
-if ($downloadsuccesful) {
-	Write-Host "Installing node.js"
-	Start-Process -FilePath msiexec.exe -ArgumentList "/i `"$env:TEMP\node-v18.20.2-x64.msi`" /qn"
-	Remove-Item -Path "$env:TEMP\node-v18.20.2-x64.msi" -Force
-} else {
-	Write-Host "Failed to download Node.js."
-}
+Invoke-WebRequest -Uri $nodeURL -OutFile "C:\Users\Administrator\node-v18.20.2-x64.msi"
+msiexec.exe  /i C:\Users\Administrator\node-v18.20.2-x64.msi /qn
 
-# Edit registry entry for auth-token-verifier
-New-ItemProperty -Force -Path "Microsoft.PowerShell.Core\Registry::\HKEY_USERS\S-1-5-18\Software\GSettings\com\nicesoftware\dcv\security" -Name "auth-token-verifier" -PropertyType "String" -Value "http://127.0.0.1:8445"
-Stop-Transcript
+# Edit registry entry
+New-ItemProperty -Path "Microsoft.PowerShell.Core\Registry::\HKEY_USERS\S-1-5-18\Software\GSettings\com\nicesoftware\dcv\security" -Name "auth-token-verifier" -PropertyType "String" -Value "http://127.0.0.1:8445" -Force
+
+echo "Copying default.perm to location C:\Program Files\NICE\DCV\Server\conf\default.perm"
+cp "C:\Users\Administrator\default.perm" "C:\Program Files\NICE\DCV\Server\conf\default.perm"
+# Add mount script and set-user-token to startup folder
+echo "copying mount_s3.bat, set_user_token.bat to startup folder"
+cp C:\Users\Administrator\mount_s3.bat "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\"
+cp C:\Users\Administrator\set_user_token.bat "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\"
+
+# Set Admin password
+net user Administrator Admin@123
+
+# End the transcript (stop logging)
+# Stop-Transcript
